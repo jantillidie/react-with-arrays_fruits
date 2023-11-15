72f7a0b06d81a26300aeff6c870ce57cc0feb375
Jan Maaß <git@antipost.de>
2023-11-15 10:36:32 +0100

    Add solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: '🍌 Banana',
+      color: 'yellow'
+    },
+    {
+      id: 1338,
+      name: '🍎 Apple',
+      color: 'red'
+    },
+    {
+      id: 1339,
+      name: '🍇 Grape',
+      color: 'purple'
+    },
+    {
+      id: 1340,
+      name: '🍊 Orange',
+      color: 'orange'
+    },
+    {
+      id: 1341,
+      name: '🍍 Pineapple',
+      color: 'brown'
+    }
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({ id, name, color }) => (
+        <Card key={id} name={name} color={color} />
+      ))}
     </div>
   );
 }
